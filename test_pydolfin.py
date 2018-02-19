7cb7c9ee30e750f89d6e85a8d1a86e74fe7a473f
Henning Bonart <bonart@tu-berlin.de>
2018-02-19 16:27:27 +0100

    Renames test file and adds test for dolfin function to omega_h. This test fails at the moment due to missing interface functionality in omega_h.

--- test_pydolfin.py
+++ test_pydolfin.py
@@ -14,3 +14,15 @@ def test_mesh():
     omega_h.mesh_to_dolfin(mesh_2, mesh_osh);
 
     assert mesh.hash() == mesh_2.hash()
+
+# TODO assert is missing
+def test_function():
+    mesh = UnitSquareMesh(32, 32)
+    mesh_osh = omega_h.new_empty_mesh()
+
+    omega_h.mesh_from_dolfin_unit_square(mesh_osh, mesh)
+
+    V = FunctionSpace(mesh, "Lagrange", 1)
+    u = Function(V)
+
+    omega_h.function_from_dolfin(mesh_osh, u, "u")
