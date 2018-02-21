150e8530190442a0825cffe25f9f47bdb46539a3
Dan Ibanez <daibane@sandia.gov>
2018-02-21 14:51:30 -0700

    aaaaand it works!

--- demo_poisson.py
+++ demo_poisson.py
@@ -13,51 +13,50 @@ g = Expression("sin(5*x[0])", degree=2)
 
 mesh_osh = omega_h.new_empty_mesh()
 omega_h.mesh_from_dolfin(mesh_osh, mesh)
-print('before first set_parting')
+omega_h.classify_by_angles(mesh_osh, 3.14159 / 4.0)
 mesh_osh.set_parting(omega_h.GHOSTED, 1)
 omega_h.add_implied_metric_tag(mesh_osh);
-print('before second set_parting')
 mesh_osh.set_parting(omega_h.ELEM_BASED, 0);
 
 i = 0
 n = 3
-print('before adapt while loop')
 while (True):
-    omega_h.mesh_to_dolfin(mesh, mesh_osh);
-    V = FunctionSpace(mesh, "Lagrange", 1)
+  omega_h.mesh_to_dolfin(mesh, mesh_osh);
+  V = FunctionSpace(mesh, "Lagrange", 1)
 
-    bc = DirichletBC(V, u0, boundary)
+  bc = DirichletBC(V, u0, boundary)
 
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    a = inner(grad(u), grad(v))*dx
-    L = f*v*dx + g*v*ds
+  u = TrialFunction(V)
+  v = TestFunction(V)
+  a = inner(grad(u), grad(v))*dx
+  L = f*v*dx + g*v*ds
 
-    u = Function(V)
-    solve(a == L, u, bc)
+  u = Function(V)
+  solve(a == L, u, bc)
 
-    omega_h.function_from_dolfin(mesh_osh, u._cpp_object, "u")
+  omega_h.function_from_dolfin(mesh_osh, u._cpp_object, "u")
 
-    file = File("poisson.pvd")
-    file << u
+  file = File("poisson.pvd")
+  file << u
 
-    if (++i == n):
-      break
+  i = i + 1
+  if (i == n):
+    break
 
-    mesh_osh.set_parting(omega_h.GHOSTED, 1)
+  mesh_osh.set_parting(omega_h.GHOSTED, 1)
 
-    source = omega_h.MetricSource(omega_h.VARIATION, 2e-3, "u")
+  source = omega_h.MetricSource(omega_h.VARIATION, 2e-3, "u")
 
-    metric_input = omega_h.MetricInput()
-    metric_input.add_source(source)
-    metric_input.should_limit_lengths = True
-    metric_input.max_length = 1.0 / 2.0
-    metric_input.should_limit_gradation = True
+  metric_input = omega_h.MetricInput()
+  metric_input.add_source(source)
+  metric_input.should_limit_lengths = True
+  metric_input.max_length = 1.0 / 2.0
+  metric_input.should_limit_gradation = True
 
-    omega_h.generate_target_metric_tag(mesh_osh, metric_input)
+  omega_h.generate_target_metric_tag(mesh_osh, metric_input)
 
-    opts = omega_h.AdaptOpts(mesh_osh)
-    opts.verbosity = omega_h.EXTRA_STATS
+  opts = omega_h.AdaptOpts(mesh_osh)
+  opts.verbosity = omega_h.EXTRA_STATS
 
-    while (omega_h.approach_metric(mesh_osh, opts)):
-      omega_h.adapt(mesh_osh, opts)
+  while (omega_h.approach_metric(mesh_osh, opts)):
+    omega_h.adapt(mesh_osh, opts)
